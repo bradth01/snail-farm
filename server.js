cf92d42a4698e0b13ade97e35ab49107e381867a
Tom Bradley <tbradley@moneylion.com>
2017-06-10 14:05:47 -0400

    can serve images and logo was added

--- server.js
+++ server.js
@@ -1,19 +1,56 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 const port = 4200;
 
-const server = http.createServer(function (request, response) {
-    fs.readFile('./index.html', function(error, content) {
+http.createServer(function (request, response) {
+    console.log('request ', request.url);
+
+    var filePath = '.' + request.url;
+    if (filePath === './') filePath = './index.html';
+
+    console.log('filePath', filePath);
+
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var contentType = 'text/html';
+    var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.svg': 'application/image/svg+xml'
+    };
+
+    contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function(error, content) {
         if (error) {
-            response.writeHead(500);
-            response.end();
-        } else {
-            response.writeHead(200);
-            response.end(content);
+            if(error.code === 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end();
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
         }
     });
-});
 
-server.listen(port, (function () {
-    console.log('listening on port ' + port);
-}));
+}).listen(port);
+console.log('listening on port ' + port);
